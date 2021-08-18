aa830cc3688c8de13d5f93b4c2645eab35d879d8
AlexYurchenk <yurchenkoalex13@gmail.com>
2021-08-18 21:45:06 +0300

    dgfdf

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,6 +2,7 @@ import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import * as actions from "../../redux/phonebook/phonebook-actions";
 import { useSelector, useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
 
 const useStyles = makeStyles({
   TextField: {
@@ -11,7 +12,13 @@ const useStyles = makeStyles({
 });
 export default function Filter({ onFilterInputChange }) {
   const dispatch = useDispatch();
-
+  const [contacts, setContacts] = useState(null);
+  useEffect(() => {
+    const arr = fetch("http://localhost:3000/contacts").then((r) =>
+      r.json().then((r) => setContacts(r))
+    );
+  }, []);
+  console.log(contacts);
   const classes = useStyles();
   const filter = useSelector((state) => state.contacts.filter);
   return (
